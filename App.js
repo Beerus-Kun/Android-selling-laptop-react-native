ae4ef42b214da5e30f2f6f7fe8a90d969c8531c3
Beerus-Kun <67775920+Beerus-Kun@users.noreply.github.com>
2021-10-31 23:27:59 +0700

    build app selling laptop react-native

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import DrawerScene from './Components/DrawerNavigation/DrawerScene';
+import DetailProduct from './Components/StackNavigation/Product/DetailProduct';
+import SearchProduct from './Components/StackNavigation/Product/SearchProduct';
+import ConfirmCart from './Components/StackNavigation/Cart/ConfirmCart';
+import Global from './Global';
+import DetailBill from './Components/StackNavigation/Bill/DetailBill';
+import ForgotPassword from './Components/StackNavigation/Password/ForgotPassword';
+import UpdatePassword from './Components/StackNavigation/Password/UpdatePassword';
+
+const Stack = createNativeStackNavigator();
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='root'
+            component={DrawerScene}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name='detailProduct'
+            component={DetailProduct}
+            options={{ title: 'Sản phẩm' }}
+          />
+
+          <Stack.Screen
+            name='searchProduct'
+            component={SearchProduct}
+            options={{ title: 'Sản phẩm' }}
+          />
+
+          <Stack.Screen
+            name='confirmCart'
+            component={ConfirmCart}
+            options={{ title: 'Thanh toán' }}
+          />
+
+          <Stack.Screen
+            name='detailBill'
+            component={DetailBill}
+            options={{ title: 'Chi tiết đơn hàng' }}
+          />
+
+          <Stack.Screen
+            name='forgotPassword'
+            component={ForgotPassword}
+            options={{ title: 'Quên mật khẩu' }}
+          />
+
+          <Stack.Screen
+            name='updatePassword'
+            component={UpdatePassword}
+            options={{ title: 'Thay đổi mật khẩu' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+
+  }
+
+  componentDidMount() {
+    Global.init()
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('buoi6-2', () => App);
